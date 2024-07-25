4ffd5493837c8a1af9e126300004a2f26bbeeb2c
Robert Martinez <robmartinez023@gmail.com>
2024-07-24 23:25:46 -0600

    Ejercicio 4 de la guía de ejercicios del bloque 1

---
+++ Ejercicio4_Guía_Bloque1/Ejercicio4_Guía_Bloque1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4_Guía_Bloque1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número para verificar si es primo: ");
+            int number = int.Parse(Console.ReadLine());
+
+            {
+                if (number <= 1)
+                {
+                    Console.WriteLine("El numero no es primo porque los números menores o iguales a 1 no son primos");
+                }
+
+               else (
+                        {
+                    Console.WriteLine("El numero que ingresó es primo");*/
+                
+
+
+                
+               
+            }
+        }
+    }
+}
---
+++ Ejercicio5_Guia_Bloque1/Ejercicio5_Guia_Bloque1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5_Guia_Bloque1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de las variables a usar
+            int num1, num2, num3, num4;
+            double promedio;
+            //Se pide al usuario que ingrese los datos
+            
+           
+                Console.WriteLine("Ingrese un numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese un numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese un numero: ");
+                num3 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese un numero: ");
+                num4 = int.Parse(Console.ReadLine());
+
+            promedio = Convert.ToDouble(num1+num2+num3+num4) / 4;
+            Console.WriteLine("El promedio de los numeros ingresados es: " + promedio);
+            Console.ReadKey();
+
+        }
+    }
+}
