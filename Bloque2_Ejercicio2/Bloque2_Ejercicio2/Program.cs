847963d392ce3c8b566b5a0a3d27d05e2fd1c620
Robert Martinez <robmartinez023@gmail.com>
2024-07-28 15:15:11 -0600

    Ejercicio 2, Guía Bloque 2.

---
+++ Bloque2_Ejercicio2/Bloque2_Ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bloque2_Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se definen las notas 
+            List<int> notas = new List<int> { 9, 5, 10, 7, 9, 10, 6, 9 };
+
+            // Uso de LINQ para filtrar las notas sobresalientes
+            var notas_sobresalientes = from nota in notas
+                                      where nota >= 8
+                                      select nota;
+
+            // Se muestran las notas sobresalientes
+            Console.WriteLine("Notas sobresalientes:");
+            foreach (var nota in notas_sobresalientes)
+            {
+                Console.WriteLine(nota);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
